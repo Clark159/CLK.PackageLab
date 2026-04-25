3117a3f57ad37268c312559a80a86cfe278c4df2
CLARK-HOME\clark <clark159@hotmail.com>
2026-04-25 10:37:03 +0800

    x

--- Maven/maven-resolve-packages.ps1
+++ Maven/maven-resolve-packages.ps1
@@ -64,7 +64,52 @@ do {
     Write-Host "[INFO] version: $PROJECT_VERSION"
     Write-Host "[INFO] ------------------------------------------------------------------------"
 
-    
+    # 產生 BOM XML
+    $bomLines = [System.Collections.Generic.List[string]]::new()
+    $bomLines.Add('<?xml version="1.0" encoding="UTF-8"?>')
+    $bomLines.Add('<project xmlns="http://maven.apache.org/POM/4.0.0"')
+    $bomLines.Add('         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"')
+    $bomLines.Add('         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">')
+    $bomLines.Add('')
+    $bomLines.Add("    <modelVersion>$PROJECT_MODELVERSION</modelVersion>")
+    $bomLines.Add("    <groupId>$PROJECT_GROUPID</groupId>")
+    $bomLines.Add("    <artifactId>$PROJECT_ARTIFACTID</artifactId>")
+    $bomLines.Add("    <version>$PROJECT_VERSION</version>")
+    $bomLines.Add('    <packaging>pom</packaging>')
+    $bomLines.Add('')
+
+    $bomLines.Add('    <dependencyManagement>')
+    $bomLines.Add('        <dependencies>')
+    foreach ($depLine in $packagesLockString) {
+        $parts = $depLine -split ':'
+        if ($parts.Count -ge 4) {
+            $depGroupId    = $parts[0]
+            $depArtifactId = $parts[1]
+            $depType       = $parts[2]
+            $depVersion    = $parts[3]
+            $depScope      = if ($parts.Count -ge 5) { $parts[4].Trim() } else { 'compile' }
+            $bomLines.Add('            <dependency>')
+            $bomLines.Add("                <groupId>$depGroupId</groupId>")
+            $bomLines.Add("                <artifactId>$depArtifactId</artifactId>")
+            $bomLines.Add("                <version>$depVersion</version>")
+            if ($depType -ne 'jar') { 
+                $bomLines.Add("                <type>$depType</type>") 
+            }
+            if ($depScope -ne 'compile') { 
+                $bomLines.Add("                <scope>$depScope</scope>") 
+            }
+            $bomLines.Add('            </dependency>')
+        }
+    }
+    $bomLines.Add('        </dependencies>')
+    $bomLines.Add('    </dependencyManagement>')
+
+    $bomLines.Add('</project>')
+    $bomLines | Set-Content 'packages-lock.xml' -Encoding UTF8
+    Write-Host "[INFO] 已產生 packages-lock.xml"
+    Write-Host "[INFO] ------------------------------------------------------------------------"
+
+
 # ===== End =====
 } while ($false)
 if ($exitCode -eq 0) {
