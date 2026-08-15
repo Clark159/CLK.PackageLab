a97a3ec2914a8a0eee9da2e6bb6ad9f5170f2804
CLARK-HOME\clark <clark159@hotmail.com>
2026-08-15 12:37:44 +0800

    x

--- NPM/npm-fetch-packages.ps1
+++ NPM/npm-fetch-packages.ps1
@@ -95,24 +95,26 @@ do {
     }
 
     # 建立 .npmrc (npmSourceList:scope-registry)
-    foreach ($npmSource in $npmSourceList) {
-        foreach ($dependency in $dependencyList) {
-            $scope = ($dependency.Name -split '/')[0]
-            $encodedName = $dependency.Name -replace '/', '%2F'
-            if ($dependency.Name -notmatch '^@[^/]+/') { continue }
-            if ($dependency.Value -notmatch '^\d+\.\d+\.\d+') { continue }
-            if ($npmrcContent | Where-Object { $_ -like "$scope`:registry=*" }) { continue }
-            $tarballUrl = "$($npmSource.url.TrimEnd('/'))/$encodedName/-/$($dependency.Name.Split('/')[-1])-$($dependency.Value).tgz"
-            $isExisting = $true
-            try {
-                $null = Invoke-WebRequest -Uri $tarballUrl -Method Head -UseBasicParsing -TimeoutSec 15 -ErrorAction Stop
-            } catch {
-                if ($_.Exception.Response -and $_.Exception.Response.StatusCode.value__ -eq 404) {
-                    $isExisting = $false
+    if ($npmSourceList.Count -gt 1) {
+        foreach ($npmSource in $npmSourceList) {
+            foreach ($dependency in $dependencyList) {
+                $scope = ($dependency.Name -split '/')[0]
+                $encodedName = $dependency.Name -replace '/', '%2F'
+                if ($dependency.Name -notmatch '^@[^/]+/') { continue }
+                if ($dependency.Value -notmatch '^\d+\.\d+\.\d+') { continue }
+                if ($npmrcContent | Where-Object { $_ -like "$scope`:registry=*" }) { continue }
+                $tarballUrl = "$($npmSource.url.TrimEnd('/'))/$encodedName/-/$($dependency.Name.Split('/')[-1])-$($dependency.Value).tgz"
+                $isExisting = $true
+                try {
+                    $null = Invoke-WebRequest -Uri $tarballUrl -Method Head -UseBasicParsing -TimeoutSec 15 -ErrorAction Stop
+                } catch {
+                    if ($_.Exception.Response -and $_.Exception.Response.StatusCode.value__ -eq 404) {
+                        $isExisting = $false
+                    }
                 }
-            }
-            if ($isExisting) {
-                $npmrcContent.Add("$scope`:registry=$($npmSource.url.TrimEnd('/'))/")
+                if ($isExisting) {
+                    $npmrcContent.Add("$scope`:registry=$($npmSource.url.TrimEnd('/'))/")
+                }
             }
         }
     }
